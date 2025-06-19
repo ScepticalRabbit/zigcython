491b67c16b52d23f157b9b19dc898547824e483c
ScepticalRabbit <thescepticalrabbit@gmail.com>
2025-06-19 08:12:09 +0100

    Working zig compilation with struct

--- src/zigcython/cython/zcyth.py
+++ src/zigcython/cython/zcyth.py
@@ -14,10 +14,10 @@ def add_vec(v0: cython.double[::1],
     v_out_np = np.full((v_len,),0.0,dtype=np.float64)
     v_out: cython.double[::1] = v_out_np
 
-    zigarray.addVec(cython.address(v0[0]),
-                    cython.address(v1[0]),
-                    cython.address(v_out[0]),
-                    v_len)
+    zigarray.zigAddVec(cython.address(v0[0]),
+                       cython.address(v1[0]),
+                       cython.address(v_out[0]),
+                       v_len)
 
     return v_out_np
 
--- src/zigcython/cython/zigarray.pxd
+++ src/zigcython/cython/zigarray.pxd
@@ -1,4 +1,9 @@
 cdef extern from "zigarray.h":
+    ctypedef struct Data:
+        double valf
+        int vali
+        size_t valu
+
     void addVec(const double* v0, const double* v1, double* v_out, size_t len)
 
 
